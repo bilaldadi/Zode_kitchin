151c45bfcd6fc015869d0a2b68ac04d242cd56a3
bilaldadi <bilaldadi963@gmail.com>
2024-07-29 13:12:42 +0300

    update cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -25,7 +25,7 @@ export function Cart() {
     const handleCompleteOrder = () => {
         console.log('Order Completed');
         navigate('/account', { state: { orderCompleted: true } });
-        // After a short delay, clear the state to avoid showing the toast on page refresh
+        
         setTimeout(() => {
                 navigate('/account', { state: { orderCompleted: false } });
             }, 500);
@@ -38,77 +38,81 @@ export function Cart() {
         window.scrollTo(0, 0)
       }, [])
     
-      if(totalItems === 0){
-        return (
-          <div className="cart" data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="300">
-              <div className="cart-page-container">
-                  <div className="cart-container">
+    //   if(totalItems === 0){
+    //     return (
+    //       <div className="cart" data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="300">
+    //           <div className="cart-page-container">
+    //               <div className="cart-container">
+    //                 <div className="cart-heading">
+    //                     <FontAwesomeIcon icon={faCartShopping} style={{fontSize: 'clamp(1rem, 1vw + 0.5rem, 1.5rem)'}} />
+    //                     <h2>My Cart</h2>
+    //                 </div>
+    //                 <div className="cart-items">
+    //                       <p>Your cart is empty</p>
+    //                       <button className="checkout-button" onClick = {() => navigate('/')} >Order Now</button>
+    //                 </div>
+    //               </div>
+    //           </div>
+    //       </div>
+    //   );
+    //   }
+
+      
+    return (
+        <div className="cart" data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="300">
+            <div className="cart-page-container">
+                <div className="cart-container">
                     <div className="cart-heading">
-                        <FontAwesomeIcon icon={faCartShopping} style={{fontSize: 'clamp(1rem, 1vw + 0.5rem, 1.5rem)'}} />
+                        <FontAwesomeIcon icon={faCartShopping} style={{ fontSize: 'clamp(1rem, 1vw + 0.5rem, 1.5rem)' }} />
                         <h2>My Cart</h2>
                     </div>
-                    <div className="cart-items">
-                          <p>Your cart is empty</p>
-                          <button className="checkout-button" onClick = {() => navigate('/')} >Order Now</button>
-                    </div>
-                  </div>
-              </div>
-          </div>
-      );
-      }
-
-      console.log(cartItems);
-    return (
-        <div>
-            
-            <div className="cart" data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="300">
-                <div className="cart-page-container">
-                    
-                    <div className="cart-container">
-                    <div className="cart-heading">
-                            <h2>My Cart</h2>
-                            <FontAwesomeIcon icon={faCartShopping} style={{fontSize: 'clamp(1rem, 1vw + 0.5rem, 1.5rem)'}} />
-                        </div>
+                    {totalItems === 0 ? (
                         <div className="cart-items">
-                            {cartItems.map((item, index) => (
-                                <div key={index} className="cart-item">
-                                    <div className="item-details">
-                                    
-                                        <img className="item-image" src={item.img_url} alt={item.name} />
-                                        <p className="item-name">{item.name}</p>
-                                        <p className="item-price">SAR {item.price}</p>
-                                        <p className="item-preference">Preference: {item.preference}</p>
-                                    </div>
-                                    <div className="item-quantity">
-                                        <button
-                                            className="quantity-button"
-                                            onClick={() => handleQuantityChange(item.id, item.preference, -1)}
-                                        >
-                                            -
-                                        </button>
-                                        <span className="quantity-number">{item.quantity}</span>
-                                        <button
-                                            className="quantity-button"
-                                            onClick={() => handleQuantityChange(item.id, item.preference, 1)}
-                                        >
-                                            +
-                                        </button>
-                                    </div>
-                                    <button
-                                        className="delete-button"
-                                        onClick={() => handleRemoveItem(item.id, item.preference)}
-                                    >
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
-                                </div>
-                            ))}
+                            <p>Your cart is empty</p>
+                            <button className="checkout-button" onClick={() => navigate('/')}>Order Now</button>
                         </div>
-                        <div className="cart-summary">
-                            <p className="total-label">Total:</p>
-                            <p className="total-price">SAR {cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
-                        </div>
-                        <button className="checkout-button" onClick={handleCompleteOrder} >Send Order</button>
-                    </div>
+                    ) : (
+                        <>
+                            <div className="cart-items">
+                                {cartItems.map((item, index) => (
+                                    <div key={index} className="cart-item">
+                                        <div className="item-details">
+                                            <img className="item-image" src={item.img_url} alt={item.name} />
+                                            <p className="item-name">{item.name}</p>
+                                            <p className="item-price">SAR {item.price}</p>
+                                            <p className="item-preference">Preference: {item.preference}</p>
+                                        </div>
+                                        <div className="item-quantity">
+                                            <button
+                                                className="quantity-button"
+                                                onClick={() => handleQuantityChange(item.id, item.preference, -1)}
+                                            >
+                                                -
+                                            </button>
+                                            <span className="quantity-number">{item.quantity}</span>
+                                            <button
+                                                className="quantity-button"
+                                                onClick={() => handleQuantityChange(item.id, item.preference, 1)}
+                                            >
+                                                +
+                                            </button>
+                                        </div>
+                                        <button
+                                            className="delete-button"
+                                            onClick={() => handleRemoveItem(item.id, item.preference)}
+                                        >
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="cart-summary">
+                                <p className="total-label">Total:</p>
+                                <p className="total-price">SAR {cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
+                            </div>
+                            <button className="checkout-button" onClick={handleCompleteOrder}>Send Order</button>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
